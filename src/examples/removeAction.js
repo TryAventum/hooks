3915f9462495e8d354a858c35b12a8967b737a4c
Mohammed Al-Mahdawi <mohammed@al-mahdawi.com>
2019-07-22 19:07:46 +0300

    Change some names

--- src/examples/removeAction.js
+++ src/examples/removeAction.js
@@ -7,39 +7,39 @@ var hooks = AventumHooks.createHooks()
  */
 
 hooks.addAction(
-  'awesome_action',
+  'AwesomeAction',
   'vendor1/plugin/function',
-  (content, arg1, arg2) => {
+  (arg1, arg2, arg3) => {
     return new Promise(function(resolve, reject) {
       setTimeout(function() {
-        console.log('action1', content, arg1, arg2)
-        resolve(content)
+        console.log('Action1', arg1, arg2, arg3)
+        resolve(arg1)
       }, 300)
     })
   },
   10
 )
 hooks.addAction(
-  'awesome_action',
+  'AwesomeAction',
   'vendor2/plugin/function',
-  (content, arg1, arg2) => {
+  (arg1, arg2, arg3) => {
     return new Promise(function(resolve, reject) {
       setTimeout(function() {
-        console.log('action2', content, arg1, arg2)
-        resolve(content)
+        console.log('Action2', arg1, arg2, arg3)
+        resolve(arg1)
       }, 300)
     })
   },
   10
 )
 
-hooks.removeAction( 'awesome_action', 'vendor1/plugin/function' )
+hooks.removeAction( 'AwesomeAction', 'vendor1/plugin/function' )
 
 /**
  * This function will run the async filters and actions
  */
 const AsyncFunction = async () => {
-  await hooks.doAction('awesome_action', 25, 6, 30)
+  await hooks.doAction('AwesomeAction', 25, 6, 30)
 }
 
 AsyncFunction()
@@ -48,30 +48,30 @@ AsyncFunction()
  * Synchronous Actions
  */
 hooks.addAction(
-  'awesome_action_sync',
+  'AwesomeActionSync',
   'vendor3/plugin/function',
   (arg1, arg2) => {
-    console.log('awesome_action_sync1', arg1, arg2)
+    console.log('AwesomeActionSync1', arg1, arg2)
   },
   10
 )
 hooks.addAction(
-  'awesome_action_sync',
+  'AwesomeActionSync',
   'vendor4/plugin/function',
   (arg1, arg2) => {
-    console.log('awesome_action_sync2', arg1, arg2)
+    console.log('AwesomeActionSync2', arg1, arg2)
   },
   10
 )
 hooks.addAction(
-  'awesome_action_sync',
+  'AwesomeActionSync',
   'vendor5/plugin/function',
   (arg1, arg2) => {
-    console.log('awesome_action_sync3', arg1, arg2)
+    console.log('AwesomeActionSync3', arg1, arg2)
   },
   10
 )
 
-hooks.removeAction( 'awesome_action_sync', 'vendor4/plugin/function' )
+hooks.removeAction( 'AwesomeActionSync', 'vendor4/plugin/function' )
 
-hooks.doActionSync('awesome_action_sync', 10, 20)
+hooks.doActionSync('AwesomeActionSync', 10, 20)
